bb44349d707c37a84180cb4bf0f7313070455ed9
John Murenya <john.murenya@gmail.com>
2022-04-24 16:56:44 +0300

    github page

--- src/App.js
+++ src/App.js
@@ -6,112 +6,112 @@ import { useEffect } from "react";
 import axios from "axios";
 
 const App = () => {
-  let i = 1;
+    let i = 1;
 
-  const getTodos = () => {
-    i = 0;
-    var config = {
-      method: "get",
-      url: "https://backend-todo-labashinskiy.herokuapp.com/get-all",
-      headers: {
-        "Access-Control-Allow-Origin": "true",
-        "Access-Control-Allow-Headers": "Origin, X-Requested, Content-Type, Accept Authorization",
-        "Access-Control-Allow-Methods": "POST, PUT, PATCH, GET, DELETE"
-      }
+    const getTodos = () => {
+        i = 0;
+        var config = {
+            method: "get",
+            url: "https://todolistappweb.herokuapp.com/get-all",
+            headers: {
+                "Access-Control-Allow-Origin": "true",
+                "Access-Control-Allow-Headers": "Origin, X-Requested, Content-Type, Accept Authorization",
+                "Access-Control-Allow-Methods": "POST, PUT, PATCH, GET, DELETE"
+            }
+        };
+        axios.defaults.headers.post['Access-Control-Allow-Origin'] = 'true';
+        axios(config)
+            .then(function(response) {
+                localStorage.setItem("store", JSON.stringify(response.data));
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+        return localStorage.getItem("store");
     };
-    axios.defaults.headers.post['Access-Control-Allow-Origin'] = 'true';
-    axios(config)
-      .then(function (response) {
-        localStorage.setItem("store", JSON.stringify(response.data));
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-    return localStorage.getItem("store");
-  };
 
-  const todosStorage = i ? getTodos() : todosStorage;
+    const todosStorage = i ? getTodos() : todosStorage;
 
-  const [todos, setTodos] = useState(JSON.parse(todosStorage) || []);
+    const [todos, setTodos] = useState(JSON.parse(todosStorage) || []);
 
-  const [finalTodos, setFinalTodos] = useState([]);
+    const [finalTodos, setFinalTodos] = useState([]);
 
-  const [status, setStatus] = useState("active");
+    const [status, setStatus] = useState("active");
 
-  useEffect(() => {
-    localStorage.setItem("store", JSON.stringify(todos));
-    const filtered = todos.filter((todo) => {
-      return todo.status === status;
-    });
-    setFinalTodos(filtered);
-  }, [status, todos]);
+    useEffect(() => {
+        localStorage.setItem("store", JSON.stringify(todos));
+        const filtered = todos.filter((todo) => {
+            return todo.status === status;
+        });
+        setFinalTodos(filtered);
+    }, [status, todos]);
 
-  const handleAddTodo = (todoName) => {
-    if (todoName === "") {
-      alert("Cannot be blank");
-    } else {
-      const cur_id = Date.now();
+    const handleAddTodo = (todoName) => {
+        if (todoName === "") {
+            alert("Cannot be blank");
+        } else {
+            const cur_id = Date.now();
 
-      fetch("https://backend-todo-labashinskiy.herokuapp.com/save", {
-        mode: "no-cors",
-        method: "POST",
+            fetch("https://todolistappweb.herokuapp.com/save", {
+                mode: "no-cors",
+                method: "POST",
 
-        body: JSON.stringify({
-          _id: cur_id,
-          name: todoName,
-          status: "active",
-        }),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8",
-        },
-      }).then(console.log);
+                body: JSON.stringify({
+                    _id: cur_id,
+                    name: todoName,
+                    status: "active",
+                }),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8",
+                },
+            }).then(console.log);
 
-      setTodos([...todos, { _id: cur_id, name: todoName, status: "active" }]);
-    }
-  };
+            setTodos([...todos, { _id: cur_id, name: todoName, status: "active" }]);
+        }
+    };
 
-  const handleDeleteTodo = (mainTodo) => {
-    fetch("https://backend-todo-labashinskiy.herokuapp.com/delete-by-id", {
-      mode: "no-cors",
-      method: "POST",
+    const handleDeleteTodo = (mainTodo) => {
+        fetch("https://todolistappweb.herokuapp.com/delete-by-id", {
+            mode: "no-cors",
+            method: "POST",
 
-      body: JSON.stringify({
-        _id: mainTodo._id,
-      }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    }).then(console.log);
+            body: JSON.stringify({
+                _id: mainTodo._id,
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            },
+        }).then(console.log);
 
-    const filteredTodos = todos.filter((todo) => {
-      return todo._id !== mainTodo._id;
-    });
-    setTodos([...filteredTodos]);
-  };
+        const filteredTodos = todos.filter((todo) => {
+            return todo._id !== mainTodo._id;
+        });
+        setTodos([...filteredTodos]);
+    };
 
-  const handleChangeStatus = (mainTodo) => {
-    console.log(mainTodo);
-    fetch("https://backend-todo-labashinskiy.herokuapp.com/update", {
-      method: "POST",
+    const handleChangeStatus = (mainTodo) => {
+        console.log(mainTodo);
+        fetch("https://todolistappweb.herokuapp.com/update", {
+            method: "POST",
 
-      body: JSON.stringify({
-        _id: mainTodo._id,
-        name: mainTodo.name,
-        status: mainTodo.status,
-      }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    }).then(console.log);
+            body: JSON.stringify({
+                _id: mainTodo._id,
+                name: mainTodo.name,
+                status: mainTodo.status,
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            },
+        }).then(console.log);
 
-    const filteredTodos = todos.filter((todo) => {
-      return todo._id !== mainTodo._id;
-    });
+        const filteredTodos = todos.filter((todo) => {
+            return todo._id !== mainTodo._id;
+        });
 
-    setTodos([...filteredTodos, mainTodo]);
-  };
+        setTodos([...filteredTodos, mainTodo]);
+    };
 
-  return (
+    return (
     <div className="wrapper">
       <div className="app">
         <Header setStatus={setStatus} status={status} />
